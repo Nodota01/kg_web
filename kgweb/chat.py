809b3fa321687ed55a2ef0da4c4309e1d85fa39a
nodota <nodota01@outlook.com>
2023-07-25 16:40:21 +0800

    complete chat

--- kgweb/chat.py
+++ kgweb/chat.py
@@ -1,9 +1,10 @@
 from flask import (Blueprint, current_app, render_template,
-                   request)
+                   request, send_file)
 from . import Result, model_assert
 
 bp = Blueprint('chat', __name__)
 
 @bp.route("/chat", methods = ['GET'])
 def chat():
-    return render_template('chat.html')
+    # 无需经过模板引擎渲染，否则VUE失效
+    return send_file('templates/chat.html')
